45792750d15af038e852e3d40b93a8905f9369c4
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2026-06-14 15:44:00 +0530

    .

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:3000",
       "https://code-editor-sooty-pi.vercel.app", // Lets Vercel in!
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
